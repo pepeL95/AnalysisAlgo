4fea2468ac24384cd2a39c1c4afab748477aa1fd
Jose Lopez <jose.lopezgarrido95@gmail.com>
2022-07-21 20:44:52 -0400

    test commit

--- Program.cpp
+++ Program.cpp
@@ -13,36 +13,39 @@ using namespace std;
 int memo[10][10];
 
 int T(int n, int m, std::list<int> A, std::list<int> B){
-    
-    if(n == 1 && m == 1) {
-        memo[n][m] = A.front() + B.front();
-    }
-
+    if(n == 1 && m == 1)
+        return A.front() + B.front();
     else if(n == 1) {
         // //extend A until n = m
         // for(int i = 1; i < m; i++)
         //     A.push_back(A.front());
         //maxsum across pairs
-        return *std::max_element(B.begin(), std::next(B.begin(), m)) + A.front();
-
+        return *max_element(B.begin(), next(B.begin(), m)) + A.front();
     }
-
     else if(m == 1) {
         // //extend B until m = n
         // int j = B.front();
         // for(int i = 0; i < n; i++){
         //     B.push_back(j);
         // }
-        return *std::max_element(A.begin(), std::next(A.begin(), n)) + B.front();
+        //maxsum across pairs
+        return *max_element(A.begin(), next(A.begin(), n)) + B.front();
     }
-
     else {
-        memo[n][m] = std::min(T(n, m-1, A, B),std::min(T(n-1, m, A, B), T(n-1, m-1, A, B)));
+        int extendA = T(n, m-1, A, B);
+        int keepAB = T(n-1, m-1, A, B);
+        int extendB = T(n-1, m, A, B);
+        memo[n][m] = min(extendA, min(keepAB, extendB));
+        if (memo[n][m] == keepAB)
+            puts("Keep same length");
+        else if (memo[n][m] == extendA)
+            puts("Extend A");
+        else
+            puts("extend B");
         int leftmost_sum = A.front() + B.front();
         if (leftmost_sum > memo[n][m])
             memo[n][m] = leftmost_sum;
     }
-
     return memo[n][m];
 }
 
@@ -72,6 +75,7 @@ int main()
     getInput(m, &B, &mFile);
     mFile.close();
 
+    cout << T(n, m, A, B) << endl;
     // cout<< "n = " << n << "\nm = " << m << "\nA = " << A.back() << "\nB = " << B.back() << endl;
 
 
