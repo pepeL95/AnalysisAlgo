d38ee117892cfc27d212345412df0b2b75a200b2
jeanniestvzz <55813990+jeanniestvzz@users.noreply.github.com>
2022-07-21 22:23:16 -0400

    Jeannie's commits debugging base cases and extensions

--- Program.cpp
+++ Program.cpp
@@ -11,25 +11,31 @@ using namespace std;
 
 // temporary for developing
 int memo[10][10];
+int original_N, original_M { 0 };
 
-int T(int n, int m, std::list<int> A, std::list<int> B){
-    if(n == 1 && m == 1)
-        return A.front() + B.front();
+int T(int n, int m, std::list<int>* A, std::list<int>* B){
+    if(n == 1 && m == 1) 
+    {
+         cout << "BASE CASE: n == 1 and m == 1" << endl;
+        return (*A).front() + (*B).front();
+    }
     else if(n == 1) {
         // //extend A until n = m
         // for(int i = 1; i < m; i++)
         //     A.push_back(A.front());  
         //maxsum across pairs
-        return *max_element(B.begin(), next(B.begin(), m)) + A.front();
+        cout << "BASE CASE: n == 1" << endl;
+        return *max_element((*B).begin(), next((*B).begin(), m)) + (*A).front();
     }
     else if(m == 1) {
         // //extend B until m = n
-        // int j = B.front();
+        // int j = (*B).front();
         // for(int i = 0; i < n; i++){
-        //     B.push_back(j);
+        //     (*B).push_back(j);
         // }
         //maxsum across pairs
-        return *max_element(A.begin(), next(A.begin(), n)) + B.front();
+        cout << "BASE CASE: m == 1" << endl;
+        return *max_element((*A).begin(), next((*A).begin(), n)) + (*B).front();
     }
     else {
         int extendA = T(n, m-1, A, B);
@@ -39,10 +45,18 @@ int T(int n, int m, std::list<int> A, std::list<int> B){
         if (memo[n][m] == keepAB)
             puts("Keep same length");
         else if (memo[n][m] == extendA)
+        {
+            auto A_it = next((*A).begin(), original_N - (n-1));
+            (*A).insert(A_it, *A_it);
             puts("Extend A");
+        }
         else
+        {
+            auto B_it = next((*B).begin(), original_M - (m-1));
+            (*B).insert(B_it, *B_it);
             puts("extend B");
-        int leftmost_sum = A.front() + B.front();
+        }
+        int leftmost_sum = (*A).front() + (*B).front();
         if (leftmost_sum > memo[n][m])
             memo[n][m] = leftmost_sum;
     }
@@ -63,6 +77,18 @@ void getInput(int size, std::list<int> *mList, fstream *mFile){
     }   
 }
 
+void printOutput(std::list<int> A, std::list<int> B) {
+    cout << endl << "A: ";
+    for (int a_value : A) {
+        cout << a_value << " ";
+    }
+    cout << endl << "B: ";
+    for (int b_value : B) {
+        cout << b_value << " ";
+    }
+    cout << endl << endl;
+}
+
 int main()
 {   
     int n, m;
@@ -71,12 +97,14 @@ int main()
     
     mFile.open("input.txt");
     getInput(&n, &m, &mFile);
+    original_M = m;
+    original_N = n;
     getInput(n, &A, &mFile);
     getInput(m, &B, &mFile);
     mFile.close();
     puts("testifg");
-    cout << T(n, m, A, B) << endl;
-    // cout<< "n = " << n << "\nm = " << m << "\nA = " << A.back() << "\nB = " << B.back() << endl;
-
-
+    printOutput(A, B);
+    cout << T(n, m, &A, &B) << endl;
+    printOutput(A, B);
+    // cout<< "n = " << n << "\nm = " << m << "\nA = " << (*A).back() << "\nB = " << (*B).back() << endl;
 }
